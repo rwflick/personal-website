e24d1bde39032ba9c09217b2e2fd88171b36c19d
ralphflick <rwflick@gmail.com>
2026-05-22 17:21:27 -0700

    Update for Deploy

--- django_project/settings.py
+++ django_project/settings.py
@@ -17,6 +17,7 @@ DEBUG = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "rwflick.pythonanywhere.com"]
 
 
 # Application definition
